a0b41e65a546a2802bf364ffb164274349c3dd05
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-09-14 19:45:28 +0900

    add image

--- src/routes/hello/+page.svelte
+++ src/routes/hello/+page.svelte
@@ -6,6 +6,9 @@
 <script>
     let greet = "Hello"
     let name = "World".toUpperCase();
+    let src = "https://picsum.photos/seed/picsum/200/300"
 </script>
 
 <h1>{greet.toUpperCase()} {name}!</h1>
+<img src={src} alt="{name} image" height=300 width=200 />
+
