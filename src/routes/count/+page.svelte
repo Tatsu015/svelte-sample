13fb51b0a920e6f72dce27a94ca173066a174e46
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-09-16 14:43:55 +0900

    add statement reactivity

--- src/routes/count/+page.svelte
+++ src/routes/count/+page.svelte
@@ -1,16 +1,25 @@
 
 <script>
     let count = 0
-
+    let debug = false
     $: doubleCount = count * 2;
+    $: {
+        if (debug === true){
+            console.log('[DEBUG]', count)
+        }
+    }
 
     let maybeNotChange = count * 2;
 
     function countUp(){
         count++;
     }
+    function toggleDebug(){
+        debug = !debug
+    }
 </script>
 
+<button on:click={toggleDebug}>DEBUG : {debug}</button>
 <button on:click={countUp}>Count UP!</button>
 <p>COUNT : {count}</p>
 <p>WCOUNT : {doubleCount}</p>
