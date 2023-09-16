0a1f16e3a7484718a136cd53d9f350041ca3c039
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-09-16 14:16:17 +0900

    add count up sample

---
+++ src/routes/count/+page.svelte
@@ -0,0 +1,17 @@
+
+<script>
+    let count = 0
+    function countUp(){
+        count++;
+    }
+</script>
+
+<button on:click={countUp}>Count UP!</button>
+<p>{count}</p>
+
+<style>
+    p {
+        text-align: center;
+        font-size: 10rem;
+    }
+</style>
