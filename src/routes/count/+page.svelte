532a72730ea9e6625bc0f699b854ebb6576221b0
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-09-16 14:23:39 +0900

    add reactivity

--- src/routes/count/+page.svelte
+++ src/routes/count/+page.svelte
@@ -1,17 +1,24 @@
 
 <script>
     let count = 0
+
+    $: doubleCount = count * 2;
+
+    let maybeNotChange = count * 2;
+
     function countUp(){
         count++;
     }
 </script>
 
 <button on:click={countUp}>Count UP!</button>
-<p>{count}</p>
+<p>COUNT : {count}</p>
+<p>WCOUNT : {doubleCount}</p>
+<p>not change : {maybeNotChange}</p>
 
 <style>
     p {
         text-align: center;
-        font-size: 10rem;
+        font-size: 5rem;
     }
 </style>
