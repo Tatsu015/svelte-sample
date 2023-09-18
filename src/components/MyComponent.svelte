3eadee5ac28e0be29423a1f23f2c1c5acaa4b750
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-09-18 15:11:44 +0900

    add object spread

--- src/components/MyComponent.svelte
+++ src/components/MyComponent.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+    export let id:number = 0
     export let name:string = "default name";
+    export let description:string = "default description"
 </script>
 
-<div>this component name is {name}</div>
+<div>this component id:{id} name:{name} description:{description}</div>
--- src/routes/hello/+page.svelte
+++ src/routes/hello/+page.svelte
@@ -8,13 +8,19 @@
     let greet = "Hello"
     let name = "World".toUpperCase();
     let src = "https://picsum.photos/seed/picsum/200/300"
+    const myComponentProps = {
+        id:1,
+        name:'spread',
+        description:'spreadspreadspread',
+    }
 </script>
 
 <h1>{greet.toUpperCase()} {name}!</h1>
 <img {src} alt="{name} image" height=300 width=200 />
 <p>styled text</p>
-<MyComponent name="aaa"/>
 <MyComponent />
+<MyComponent id={10} name="aaa" description="aaaaaaaaaaaaaaaaaa"/>
+<MyComponent {...myComponentProps} />
 
 <style>
     p {
