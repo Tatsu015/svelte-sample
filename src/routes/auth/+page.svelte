49af1a636b75b61b158ff85c28aaffb9e3fb04a8
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-09-24 21:05:35 +0900

    add if state

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    let loggedIn = false
+    function toggle(){
+        loggedIn  = !loggedIn;
+    }
+</script>
+
+
+{#if loggedIn == true}
+    <button on:click={toggle}>Logout</button>
+{/if}
+
+{#if loggedIn == false}
+    <button on:click={toggle}>Login</button>
+{/if}
