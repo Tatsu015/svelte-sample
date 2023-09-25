3ff06973917c07ff061c4378cac7500248abeb09
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-09-25 13:44:46 +0900

    add else if state

---
+++ src/routes/password/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    let val = ""
+    $: gval = val
+</script>
+
+<input type="text" bind:value={val}/>
+<div>
+    {#if gval.length < 4}
+        password: 短すぎます    
+    {:else if gval.length < 6}
+        password: 普通
+    {:else}
+        password: 強力
+    {/if}
+</div>
